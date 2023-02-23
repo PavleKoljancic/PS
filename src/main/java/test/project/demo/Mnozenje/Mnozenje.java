d5c65b0110816fa5349b287966a518263e907577
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-02-23 18:51:38 +0100

    Moje

---
+++ src/main/java/test/project/demo/Mnozenje/Mnozenje.java
@@ -0,0 +1,21 @@
+package test.project.demo.Mnozenje;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping(path = "api/mnozenje")
+public class Mnozenje {
+    @Autowired
+    Mnozenje( ) {}
+    @PostMapping(value="")
+    public double postMethodName(@RequestBody Operatori entity) {
+      
+        return entity.getA()*entity.getB();
+    }
+    
+}
