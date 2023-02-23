63ca07b8a3948a2786102978d983f55b9a7b3d91
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-02-23 11:16:18 +0100

    Evo sad ce valjda

---
+++ src/main/java/test/project/demo/Show/Show.java
@@ -0,0 +1,36 @@
+package test.project.demo.Show;
+
+
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/show")
+public class Show {
+	
+    
+
+	
+	@Autowired //Magicna instancijacija
+	public Show() {
+
+	}
+
+
+
+
+	@GetMapping
+	public String [] hello() 
+	{
+		String [] arr =  {"Cao","Kolege"};
+        return arr;
+	}
+
+}
