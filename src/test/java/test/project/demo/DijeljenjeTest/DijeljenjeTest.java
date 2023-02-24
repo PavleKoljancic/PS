d1364aa88cfd2260e46b67a642cc52127cb10c17
kezija99 <dejankezic99@gmail.com>
2023-02-24 17:11:22 +0100

    Dijeljenje test

---
+++ src/test/java/test/project/demo/DijeljenjeTest/DijeljenjeTest.java
@@ -0,0 +1,13 @@
+package test.project.demo.DijeljenjeTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DijeljenjeTest {
+
+    @Test
+    public  void testDijeljenja() 
+    {
+        assertEquals(2.5, 10.0/4.0);
+    }
+}
