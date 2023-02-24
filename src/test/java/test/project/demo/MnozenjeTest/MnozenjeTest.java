9bff97bb7814f3049f517b090f8889314e057e22
PavleKoljancic <pavle.koljancic@student.etf.unibl.org>
2023-02-24 17:01:08 +0100

    Prvi Test

---
+++ src/test/java/test/project/demo/MnozenjeTest/MnozenjeTest.java
@@ -0,0 +1,20 @@
+package test.project.demo.MnozenjeTest;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class MnozenjeTest {
+
+    @Test
+    public  void myTestMethod() 
+    {
+        assertEquals(5, 2+3);
+        
+    }
+
+
+    
+}
