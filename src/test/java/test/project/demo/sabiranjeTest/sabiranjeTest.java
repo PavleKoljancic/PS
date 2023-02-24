b875b64749052082e5d6babb2f4dcf9a5cf3a516
Bane <branislav619@gmail.com>
2023-02-24 17:15:26 +0100

    sabiranje test

---
+++ src/test/java/test/project/demo/sabiranjeTest/sabiranjeTest.java
@@ -0,0 +1,16 @@
+package test.project.demo.sabiranjeTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class sabiranjeTest {
+
+    @Test
+    public void TestMetoda()
+    {
+        assertEquals(5, 2+3);
+    }
+    
+}
